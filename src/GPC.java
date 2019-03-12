c58350d7269d1a57f14e78437bd79d00701c4d95
AJSinc <ajsinc@outlook.com>
2019-03-12 18:32:19 -0500

    Add files via upload

---
+++ src/GPC.java
@@ -0,0 +1,367 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GPC {
+	
+	private String gpcFilePath;
+	
+	private List<String> definedList;
+	private List<String> varList;
+	private List<String> mappingCode;
+	private List<String> initCode;
+	private List<String> mainCode;
+	private List<String> comboList;
+	private List<String> functionList;
+	private String dataSegment;
+	
+	private static Scanner gpcReader;
+	private static int readIdx;
+	private static int codeBlockBraceCount;
+	
+	public GPC(String s) {
+		definedList = new ArrayList<String>();
+		varList = new ArrayList<String>();
+		mappingCode = new ArrayList<String>();
+		initCode = new ArrayList<String>();
+		mainCode = new ArrayList<String>();
+		comboList = new ArrayList<String>();
+		functionList = new ArrayList<String>();
+		dataSegment = "";
+		gpcFilePath = s;
+		
+		readAll();
+	}
+	
+	public String getRawGPCCode() {
+		String rawCode = "";
+		try {
+			rawCode = new String(Files.readAllBytes(Paths.get(gpcFilePath)), StandardCharsets.UTF_8);
+			rawCode = removeComments(rawCode);
+			rawCode = replaceKeywords(rawCode);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rawCode;
+	}
+	
+	private void readAll() {
+		String rawGPC = getRawGPCCode();
+		gpcReader = new Scanner(rawGPC);
+		while(gpcReader.hasNextLine()) {
+			String currLine = readNextLine();
+			while(readIdx < currLine.length()) {
+				currLine = currLine.substring(readIdx).trim();
+				readIdx = 0;
+				if(currLine.startsWith("define "))  definedList.add(parseDefine(currLine));
+				else if(currLine.startsWith("unmap") || currLine.startsWith("map") || currLine.startsWith("remap")) mappingCode.add(parseMapping(currLine));
+				else if(currLine.startsWith("int ")) varList.add(parseVarType(currLine));
+				else if(currLine.startsWith("init")) initCode.add(parseCodeBlock(currLine, '{', '}'));
+				else if(currLine.startsWith("main")) mainCode.add(parseCodeBlock(currLine, '{', '}'));
+				else if(currLine.startsWith("combo ")) comboList.add(parseCodeBlock(currLine, '{', '}'));
+				else if(currLine.startsWith("function ")) functionList.add(parseCodeBlock(currLine, '{', '}'));
+				else if(currLine.startsWith("data")) dataSegment = parseCodeBlock(currLine, '(', ')') + ";"; // will cause ; to trigger other
+				
+				else { // unknown line
+					System.out.println("Other: " + currLine);
+					readIdx = currLine.length();
+				}
+				
+				if(readIdx < currLine.length()) {
+					//System.out.println(currLine);
+				}
+			}
+		}
+		replaceFunctionNames();
+		replaceComboNames();
+		gpcReader.close();
+	}
+	
+	private static String readNextLine() {
+		String str = "";
+		do {
+			str = trimSpacing(gpcReader.nextLine());
+		} while(gpcReader.hasNextLine() && str.isEmpty());
+		readIdx = 0;
+		return str;
+	}
+	
+	private static String parseVarType(String s) {
+		String parsedStr = "";
+		int i = 0;
+		while(i < s.length()) {
+			readIdx++;
+			if(s.charAt(i) == ';') break;
+			parsedStr += "" + s.charAt(i);
+			i++;
+		}
+		if(parsedStr.trim().endsWith(",")) {
+			String tmp = parsedStr.trim() + "\r\n\t" + readNextLine();
+			i = -i; // set it to negative, so it subtracts the number of chars previously read in prev lines
+			parsedStr = parseVarType(tmp);
+		}
+		return parsedStr + (!parsedStr.endsWith(";") ? ";" : "");
+	}
+	
+	private static String parseDefine(String s) {
+		String parsedStr = "";
+		int i = 0;
+		while(i < s.length()) {
+			readIdx++;
+			if(s.charAt(i) == ';') break;
+			parsedStr += "" + s.charAt(i);
+			i++;
+		}
+		if(parsedStr.trim().endsWith(",")) {
+			String tmp = parsedStr.trim();
+			tmp = tmp + readNextLine();
+			i = -i; // set it to negative, so it subtracts the number of chars previously read in prev lines
+			parsedStr = parseDefine(tmp);
+		}
+		return (parsedStr + (!parsedStr.endsWith(";") ? ";" : "")).replaceAll(",", ";\r\ndefine ");
+	}
+	
+	private static String parseCodeBlock(String s, char blockSetStart, char blockSetEnd) {
+		String parsedStr = "";
+		int i = 0;
+		while(i < s.length()) {
+			readIdx++;
+			parsedStr += "" + s.charAt(i);
+			if(s.charAt(i) == blockSetStart) {
+				codeBlockBraceCount++;
+			}
+			else if(s.charAt(i) == blockSetEnd) {
+				codeBlockBraceCount--;
+				if(codeBlockBraceCount <= 0) {
+					//if(s.charAt(s.length() - 1) == ';') readIdx++;
+					codeBlockBraceCount = -1;
+					break;
+				}
+			}
+			i++;
+		}
+		if(codeBlockBraceCount != -1) {
+			String tmp = parsedStr + "\r\n" + readNextLine();
+			i = -i; // set it to negative, so it subtracts the number of chars previously read in prev lines
+			codeBlockBraceCount = 0;
+			parsedStr = parseCodeBlock(tmp, blockSetStart, blockSetEnd);
+		}
+		codeBlockBraceCount = 0;
+		return parsedStr;
+	}
+	
+	private static String removeComments(String code) {
+	    StringBuilder newCode = new StringBuilder();
+	    try (StringReader sr = new StringReader(code)) {
+	        boolean inBlockComment = false;
+	        boolean inLineComment = false;
+	        boolean out = true;
+
+	        int prev = sr.read();
+	        int cur;
+	        for(cur = sr.read(); cur != -1; cur = sr.read()) {
+	            if(inBlockComment) {
+	                if (prev == '*' && cur == '/') {
+	                    inBlockComment = false;
+	                    out = false;
+	                }
+	            } else if (inLineComment) {
+	                if (cur == '\r') { // start untested block
+	                    sr.mark(1);
+	                    int next = sr.read();
+	                    if (next != '\n') {
+	                        sr.reset();
+	                    }
+	                    newCode.append("\r\n");
+	                    inLineComment = false;
+	                    out = false; // end untested block
+	                } 
+					else if (cur == '\n') {
+						newCode.append("\n");
+	                    inLineComment = false;
+	                    out = false;
+	                }
+	            } 
+				else {
+	                if (prev == '/' && cur == '*') {
+	                    sr.mark(1); // start untested block
+	                    //int next = sr.read();
+						// if (next != '*') 
+	                        inBlockComment = true; // tested line (without rest of block)
+	                    //
+	                    sr.reset(); // end untested block
+	                }
+					else if (prev == '/' && cur == '/') {
+	                    inLineComment = true;
+	                } 
+					else if (out){
+	                    newCode.append((char)prev);
+	                } 
+					else {
+	                    out = true;
+	                }
+	            }
+	            prev = cur;
+	        }
+	        if (prev != -1 && out && !inLineComment) {
+	            newCode.append((char)prev);
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+
+	    return newCode.toString();
+	}
+	
+	private static String trimSpacing(String s) {
+		if(s.isEmpty()) return "";
+		return s.trim().replaceAll("(\\s+)"," ");
+	}
+	
+	private static String parseMapping(String s) { 
+		String parsedStr = "";
+		int i = 0;
+		while(i < s.length()) {
+			readIdx++;
+			if(s.charAt(i) == ';') break;
+			parsedStr += "" + s.charAt(i);
+			i++;
+		}
+		// add support for multiline remaps?
+		return parsedStr + (!parsedStr.endsWith(";") ? ";" : "");
+	}
+	
+	private static String replaceKeywords(String s) {
+		File tmpDir = new File(System.getProperty("user.dir") + "\\keywords.db");
+		if(tmpDir.exists()) {
+			try {
+				Scanner kwSc = new Scanner(tmpDir);
+				while(kwSc.hasNextLine()) {
+					String str = trimSpacing(kwSc.nextLine());
+					s = s.replaceAll("\\b" + str + "\\b", "t1_" + str);
+				}
+				kwSc.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		s = s.replaceAll((":"), (";")); // : = ; in CM lool
+		s = s.replaceAll("main\\s*\\{", "main\\{");
+		s = s.replaceAll("init\\s*\\{", "init\\{");
+		s = s.replaceAll("data\\s*\\(", "data\\(");
+		s = s.replaceAll("\\bdefine\\b", "\r\ndefine");
+		s = s.replaceAll("\\bint\\b", "\r\nint");
+		s = s.replaceAll("\\binit\\b", "\r\ninit");
+		s = s.replaceAll("\\bmain\\b", "\r\nmain");
+		s = s.replaceAll("\\bcombo\\b", "\r\ncombo");
+		s = s.replaceAll("\\bfunction\\b", "\r\nfunction");
+		s = s.replaceAll("\\bdata\\b", "\r\ndata");
+		s = s.replaceAll("\\s*;", ";");
+		
+		return s;
+	}
+	
+	private void replaceComboNames() {
+		if(comboList.isEmpty()) return;
+		List<String> comboNames = getComboNames();
+		String pattern = "\\b\\s*(combo_run|combo_running|combo_stop|combo_restart|call)\\b\\s*\\(\\s*";
+		for(int i = 0; i < initCode.size(); i++) {
+			for(int k = 0; k < comboNames.size(); k++)
+				initCode.set(i, initCode.get(i).replaceAll(pattern + comboNames.get(k) + "\\s*\\)", " $1\\(c_" + comboNames.get(k)+ "\\)"));
+		}
+		for(int i = 0; i < mainCode.size(); i++) {
+			for(int k = 0; k < comboNames.size(); k++)
+				mainCode.set(i, mainCode.get(i).replaceAll(pattern + comboNames.get(k) + "\\s*\\)", " $1\\(c_" + comboNames.get(k)+ "\\)"));
+		}
+		for(int i = 0; i < comboList.size(); i++) {
+			for(int k = 0; k < comboNames.size(); k++) {
+				comboList.set(i, comboList.get(i).replaceAll(pattern + comboNames.get(k) + "\\s*\\)", " $1\\(c_" + comboNames.get(k) + "\\)"));
+				comboList.set(i, comboList.get(i).replaceAll("\\s*combo\\s*" + comboNames.get(k) + "\\s*\\{", "combo c_" + comboNames.get(k) + "\\{"));
+			}
+		}
+		for(int i = 0; i < functionList.size(); i++) {
+			for(int k = 0; k < comboNames.size(); k++)
+				functionList.set(i, functionList.get(i).replaceAll(pattern + comboNames.get(k) + "\\s*\\)", " $1\\(c_" + comboNames.get(k)+ "\\)"));
+		}
+		
+	}
+	
+	private List<String> getComboNames() {
+		List<String> comboNames = new ArrayList<String>();
+		if(!comboList.isEmpty()) {
+			for(int i = 0; i < comboList.size(); i++) {
+				String combo = comboList.get(i);
+				int lastIdx = combo.indexOf("{") != -1 ? combo.indexOf("{") : combo.length();
+				String comboName = combo.substring(combo.indexOf("combo ") + 6, lastIdx).trim();
+				comboNames.add(comboName);
+			}
+		}
+		return comboNames;
+		
+	}
+	
+	private static String fixSemicolons(String s) {
+		String fixedStr = s;
+		
+		return fixedStr;
+	}
+	
+	private void replaceFunctionNames() {
+		if(functionList.isEmpty()) return;
+		List<String> functionNames = getFunctionNames();
+		
+		for(int i = 0; i < initCode.size(); i++) {
+			for(int k = 0; k < functionNames.size(); k++)
+				initCode.set(i, initCode.get(i).replaceAll("\\b\\s*" + functionNames.get(k) + "\\b\\s*\\(", " f_" + functionNames.get(k) + "\\("));
+		}
+		for(int i = 0; i < mainCode.size(); i++) {
+			for(int k = 0; k < functionNames.size(); k++)
+				mainCode.set(i, mainCode.get(i).replaceAll("\\b\\s*" + functionNames.get(k) + "\\b\\s*\\(", " f_" + functionNames.get(k) + "\\("));
+		}
+		for(int i = 0; i < comboList.size(); i++) {
+			for(int k = 0; k < functionNames.size(); k++)
+				comboList.set(i, comboList.get(i).replaceAll("\\b\\s*" + functionNames.get(k) + "\\b\\s*\\(", " f_" + functionNames.get(k) + "\\("));
+			}
+		for(int i = 0; i < functionList.size(); i++) {
+			for(int k = 0; k < functionNames.size(); k++)
+				functionList.set(i, functionList.get(i).replaceAll("\\b\\s*" + functionNames.get(k) + "\\b\\s*\\(", " f_" + functionNames.get(k) + "\\("));	
+		}
+		
+	}
+	
+	private List<String> getFunctionNames() {
+		List<String> functionNames = new ArrayList<String>();
+		if(!functionList.isEmpty()) {
+			for(int i = 0; i < functionList.size(); i++) {
+				String function = functionList.get(i);
+				int lastIdx = function.indexOf("(") != -1 ? function.indexOf("(") : function.length();
+				String funcName = function.substring(function.indexOf("function ") + 9, lastIdx).trim();
+				functionNames.add(funcName);
+			}
+		}
+		return functionNames;
+	}
+	
+	public String toString() {
+		String str = "";
+		str += String.join("\r\n", definedList) + "\r\n\r\n";
+		str += String.join("\r\n", mappingCode) + "\r\n\r\n";
+		str += String.join("\r\n", varList) + "\r\n\r\n";
+		str += String.join("\r\n", initCode) + "\r\n\r\n";
+		str += String.join("\r\n", mainCode) + "\r\n\r\n";
+		str += String.join("\r\n", comboList) + "\r\n\r\n";
+		str += String.join("\r\n", functionList) + "\r\n\r\n";
+		str += dataSegment;
+		return str;
+	}
+}
---
+++ src/GPCtoGPC2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class GPCtoGPC2 {
+	
+	public static void main(String args[]) throws IOException {
+		File dir = new File(System.getProperty("user.dir"));
+		File[] files = dir.listFiles((d, name) -> name.endsWith(".gpc"));
+		for(int i = 0; i < files.length; i++) {
+			if(!files[i].getName().contains(".gpc2"))
+				convert(files[i]);
+		}
+	}
+	
+	public static void convert(File gpcFile) throws FileNotFoundException, IOException {
+		GPC newGPC = new GPC(gpcFile.getCanonicalPath());
+		
+		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(gpcFile.getName().substring(0, (gpcFile.getName().length() - 3)) + "gpc2.gpc"), "utf-8"));
+		writer.write("#pragma METAINFO(\"" + gpcFile.getName() +  "\", 1, 0, \"Buffy's GPC Converter\"))\r\n");
+		writer.write(
+				"#include <titanone.gph>\r\n\r\n" +
+				"#undef set_val\r\n" + 
+				//"#undef combo_run\r\n" + 
+				//"#undef combo_stop\r\n" + 
+				//"#undef combo_running\r\n" + 
+				//"//#undef combo\r\n" + 
+				"#define set_val(a,b) set_val((uint8)a,(int16)b);\r\n" + 
+				//"#define combo_run(a) combo_run(c_##a)\r\n" + 
+				//"#define combo_stop(a) combo_stop(c_##a)\r\n" + 
+				//"#define combo_running(a) (!c_##a)\r\n" + 
+				//"#define combo_suspend(a) combo_pause(c_##a)\r\n" + 
+				//"#define combo_suspended(a) (!c_##a&&_##a[1])\r\n" + 
+				"#define wait(a) ;wait((int)a);\r\n" + 
+				"#define IF(a) if(a)\r\n\r\n");
+		
+		writer.write(newGPC.toString());
+		writer.close();
+	}
+	
+	
+	
+
+	
+}
