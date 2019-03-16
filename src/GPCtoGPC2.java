a2302f5d4cce06e6ca0f0e84bcd530966293b734
AJSinc <ajsinc@outlook.com>
2019-03-16 02:48:17 -0500

    Update GPCtoGPC2.java
    - Added open file window for selecting the GPC file

--- src/GPCtoGPC2.java
+++ src/GPCtoGPC2.java
@@ -6,30 +6,50 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class GPCtoGPC2 {
 	
 	public static void main(String args[]) throws IOException {
-		File dir = new File(System.getProperty("user.dir"));
-		File[] files = dir.listFiles((d, name) -> name.endsWith(".gpc"));
-		for(int i = 0; i < files.length; i++) {
-			if(!files[i].getName().contains(".gpc2"))
-				convert(files[i]);
+		
+		
+		File gpcFile = chooseFile();
+		if(gpcFile.exists()) {
+			//System.out.print(gpcFile.getAbsoluteFile());
+			convert(gpcFile);
 		}
+		System.exit(0);
+		/*
+		File dir = new File(System.getProperty("user.dir"));
+		//File[] files = dir.listFiles((d, name) -> name.endsWith(".gpc"));
+		//for(int i = 0; i < files.length; i++) {
+			//if(!files[i].getName().contains(".gpc2"))
+				//convert(files[i]);
+		//}*/
 	}
+	
+	public static File chooseFile() {
+		JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("GPC Script files", "gpc");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(null);
+		if(returnVal == JFileChooser.APPROVE_OPTION) 
+			return chooser.getSelectedFile();
+		return null;
+	}
+	
 	
 	public static void convert(File gpcFile) throws FileNotFoundException, IOException {
 		GPC newGPC = new GPC(gpcFile.getCanonicalPath());
 		
-		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(gpcFile.getName().substring(0, (gpcFile.getName().length() - 3)) + "gpc2.gpc"), "utf-8"));
-		writer.write("#pragma METAINFO(\"" + gpcFile.getName() +  "\", 1, 0, \"Buffy's GPC Converter v0.1\"))\r\n");
+		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(gpcFile.getCanonicalPath().substring(0, (gpcFile.getCanonicalPath().length() - 3)) + "gpc2.gpc"), "utf-8"));
+		writer.write("#pragma METAINFO(\"" + gpcFile.getName() +  "\", 1, 0, \"Buffy's GPC Converter v0.11\"))\r\n");
 		writer.write("#include <titanone.gph>\r\n\r\n");
 		
 		writer.write(newGPC.toString());
 		writer.close();
 	}
 	
-	
-	
-
 	
 }
